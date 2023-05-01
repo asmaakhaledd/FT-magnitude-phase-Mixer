06f1ed79ed40db0df2217ff8ea0c8c65da6ac2e4
AbdulmonemElsherif <83321464+AbdulmonemElsherif@users.noreply.github.com>
2023-05-02 02:34:45 +0300

    spelling error

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory
-from image_processor import ImageProcessor
+from ImageProcessor import ImageProcessor
 import os
 import uuid
 
