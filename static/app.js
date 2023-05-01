1b39bffc953337a6e86b91f87c214bd7f741ac91
AbdulmonemElsherif <83321464+AbdulmonemElsherif@users.noreply.github.com>
2023-05-02 01:51:58 +0300

    a trial ui

--- static/app.js
+++ static/app.js
@@ -1,1 +1,21 @@
-    
+const mixingRatioSlider = document.getElementById("mixing-ratio");
+const mixingRatioValue = document.getElementById("mixing-ratio-value");
+
+mixingRatioValue.innerHTML = mixingRatioSlider.value + "%";
+
+mixingRatioSlider.oninput = function() {
+mixingRatioValue.innerHTML = this.value + "%";
+}
+
+const output1Button = document.getElementById("output1");
+const output2Button = document.getElementById("output2");
+
+output1Button.onclick = function() {
+document.getElementById("output").value = "1";
+document.getElementById("mixing-form").submit();
+}
+
+output2Button.onclick = function() {
+document.getElementById("output").value = "2";
+document.getElementById("mixing-form").submit();
+}
