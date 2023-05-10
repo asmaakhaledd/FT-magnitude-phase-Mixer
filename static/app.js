66ed13cc469c349ba28f36e43fd4453b3f00c914
mariamgamal70 <mariamgamal70@gmail.com>
2023-05-11 02:37:26 +0300

    added value of slider

--- static/app.js
+++ static/app.js
@@ -129,7 +129,7 @@ document.querySelectorAll(".fileinput").forEach((input,index)=>{
       } else {
         formDataMix.set("ratio2", slider.value);
       }
-      
+      document.querySelectorAll(".slidervalue")[index].innerHTML=`${slider.value}%`;
       mixImages();
     });
   });
@@ -150,84 +150,3 @@ document.querySelectorAll(".fileinput").forEach((input,index)=>{
         document.getElementById(outputSelector.value).src = url;
       });
   }
-  
-  //component mixer1----------------------------
-// imageSelectorOne.addEventListener("change", () => {
-//   imageSelectorOne.value == 1
-//     ? (componentOne.image = img1.src)
-//     : (componentOne.image = img2.src);
-// });
-
-  // componentSelectorOne.addEventListener("change", () => {
-  //   componentOne.component=componentSelectorOne.value;
-  // });
-
-  // ratioSliderOne.addEventListener("change", () => {
-  //   componentOne.component = ratioSliderOne.value;
-  // });
-
-//component mixer 2------------------------------
-// imageSelectorTwo.addEventListener("change", () => {
-//   imageSelectorTwo.value == 1
-//     ? (componentTwo.image = img1.src)
-//     : (componentTwo.image = img2.src);
-// });
-
-// componentSelectorTwo.addEventListener("change", () => {
-//   componentTwo.component = componentSelectorTwo.value;
-// });
-
-// ratioSliderTwo.addEventListener("change", () => {
-//   componentTwo.component = ratioSliderTwo.value;
-// });
-
-// document.querySelector(".selectmixeroutput").addEventListener("change",()=>{
-//   const selectedOutput = document.querySelector(".selectmixeroutput").value;
-
-// })
-
-
-// //------------------output mixer
-// const outputSelect = document.getElementById("output-select");
-// const component1Select = document.getElementById("component1-select");
-// const component1Range = document.getElementById("component1-range");
-// const component1Type = document.getElementById("component1-type");
-// const component2Select = document.getElementById("component2-select");
-// const component2Range = document.getElementById("component2-range");
-// const component2Type = document.getElementById("component2-type");
-// const output1 = document.getElementById("output1");
-// const output2 = document.getElementById("output2");
-
-// outputSelect.addEventListener("change", () => {
-//   const outputNum = outputSelect.value;
-//   const component1 = component1Select.value;
-//   const component1Value = component1Range.value;
-//   const component1TypeValue = component1Type.value;
-//   const component2 = component2Select.value;
-//   const component2Value = component2Range.value;
-//   const component2TypeValue = component2Type.value;
-
-//   fetch("/mix_images", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       output_num: outputNum,
-//       component1: component1,
-//       component1_value: component1Value,
-//       component1_type: component1TypeValue,
-//       component2: component2,
-//       component2_value: component2Value,
-//       component2_type: component2TypeValue,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       if (outputNum === "1") {
-//         output1.src = "data:image/png;base64," + data.output_image;
-//       } else if (outputNum === "2") {
-//         output2.src = "data:image/png;base64," + data.output_image;
-//       }
-//     });
-// });
